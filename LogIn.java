41bf76c2283358a1cdfcfd882ba910a3b39b9093
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-06-28 17:11:40 +0300

    bir problem olutu tekrar yükledim

--- LogIn.java
+++ LogIn.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class LogIn {
+import java.util.Scanner;
+public class LogIn {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        String userName, password;
+
+
+       // Kullanıcı girişleri
+        System.out.print("Kullanıcı adınızı giriniz: ");
+        userName = inp.nextLine();
+
+        System.out.print("Şifre: ");
+        password = inp.nextLine();
+        if(userName.equals("patika") && password.equals("java123")){
+            System.out.println("Giriş yaptınız.");
+        }else{
+            System.out.println("Gİrdiğiniz şifre veya kullanıcı adı yanlış !");
+
+        }
+
+    }
 }
