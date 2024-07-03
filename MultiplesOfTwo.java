0b775dcd994fedfce0385e723cd4d849a2b69889
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-03 14:30:48 +0300

    4'ün ve 5'in katlarını hesaplayacak şekilde değiştirildi

--- MultiplesOfTwo.java
+++ MultiplesOfTwo.java
@@ -2,13 +2,27 @@ import java.util.Scanner;
 public class MultiplesOfTwo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i, n, select;
+        int i, n, four, five;
         System.out.print("Sayı Giriniz: ");
         n = scan.nextInt();
 
+        System.out.print("4'ün katlartı: ");
         for(i = 1; i < n;){
-            System.out.println(i + ", ");
-            i *= 2;
+            four = i *= 4;
+            if(i > n){
+                break;
+            }
+            System.out.print(four + ", ");
+        }
+        System.out.println();
+
+        System.out.print("5'ün katlartı: ");
+        for(i = 1; i < n;){
+            five = i *= 5;
+            if(i > n){// bu if yapısı i n sayısından büyük olduğunda duracak ve aşağıdaki işlem devam edecek.
+                break;
+            }
+            System.out.print(five + ", ");
 
         }
     }
