2e82b651bd081b625d3f8a999a5c0d5699c01dc7
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-03 13:58:01 +0300

    değişiklik yapıldı

--- CollectOddNumbers.java
+++ CollectOddNumbers.java
@@ -6,18 +6,14 @@ public class CollectOddNumbers {
         int n, total = 0;
 
         do {
-
             System.out.println("Sayı Giriniz: ");
             n = scan.nextInt();
 
             if(n % 2 == 1){
                 total += n;
+
             }
-
         }while( n > 0);
-
         System.out.println("Toplam: " + total);
-
-
     }
 }
