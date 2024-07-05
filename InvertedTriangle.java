4a8916d2ded6c79fea9e2879b9cdc8e9efc6dcc9
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-05 15:32:44 +0300

    Ters Üçgen projesi

---
+++ InvertedTriangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class InvertedTriangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        System.out.print("Sayı giriniz: ");
+        n = scan.nextInt();
+
+        for (int i = n; i >= 0 ; i--){
+            for (int j = 0; j < 2*(n - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= (2 * i +1) ; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println(" ");
+        }
+
+    }
+}
