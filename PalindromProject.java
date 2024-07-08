9c009d184f7c3d7b38daf8805a0dd3c8fbc4a543
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-08 17:17:16 +0300

    Ödev dosyası oluşturuldu

---
+++ PalindromProject.java
@@ -0,0 +1,13 @@
+public class PalindromProject {
+    public static void main(String[] args) {
+        // Palidrom Ödevini yap
+        /*
+        Java ile bir sayının "Palindrom Sayı" olup olmadığını bulan bir program yapıyoruz.
+
+Palindrom Sayı Nedir ?
+Palindromik sayı, iki taraftan okunduğu zaman okunuş yönüyle aynı olan sayılardır.
+
+Örnek: 1, 4, 8, 99, 101, 363, 4004, 9889....
+         */
+    }
+}
