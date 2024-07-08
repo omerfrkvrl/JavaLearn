aebce5b4ad6eb35217b2dc008800d6115dcd983e
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-08 17:25:40 +0300

    Gelişmiş Hesap Makinesi Programı Dosyası oluşturuldu

---
+++ AdvancedCalculator.java
@@ -0,0 +1,19 @@
+public class AdvancedCalculator {
+    public static void main(String[] args) {
+        /*
+        Gelişmiş Hesap Makinesi
+Java ile kullanıcın seçtiği işlemleri yapan hesap makinesi yapıyoruz.
+
+Hesap makinesinin fonksiyonları :
+
+1- Toplama İşlemi
+2- Çıkarma İşlemi
+3- Çarpma İşlemi
+4- Bölme işlemi
+5- Üslü Sayı Hesaplama
+6- Faktoriyel Hesaplama
+7- Mod Alma
+8- Dikdörtgen Alan ve Çevre Hesabı
+         */
+    }
+}
