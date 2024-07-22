a169d6e6860d3ccbb1f9ff2e6ff82f8b4ae3a4e0
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-22 15:38:12 +0300

    update

--- HarmonicMeanArray.java
+++ HarmonicMeanArray.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class HarmonicMeanArray {
+import java.util.Arrays;
+public class HarmonicMeanArray {
+    public static void main(String[] args) {
+        int[] num = {1,2,3,4,5};
+        double sum = 0.0;
+        double avarage = 0.0;
+
+        for (int i = 0; i < num.length; i++){
+            sum += (1/num[i]);
+        }
+        avarage = num.length / sum;
+        System.out.println(sum);
+        System.out.println("Harmonik Ortalama: " + avarage);
+
+    }
 }
