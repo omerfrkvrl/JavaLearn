24e3d51d717fd0468dc2ff8ef479b7fa0d508fb6
ÖMER FARUK VAROL <omerfarukvarol@192.168.1.144>
2024-07-23 14:17:00 +0300

    Dizideki elemanların kaçkere tekrar ettiğini bulan algoritma

--- ElementFrequencyArray.java
+++ ElementFrequencyArray.java
@@ -1,2 +1,21 @@
 public class ElementFrequencyArray {
+
+    public static void main(String[] args) {
+        int n = 0;
+        int[] list = {10, 20, 20, 10, 10, 20, 5, 20};
+        boolean[] checked = new boolean[list.length];
+
+        for (int i = 0; i < list.length; i++) {
+            if (!checked[i]) {
+                for (int j = 0; j < list.length; j++) {
+                    if (list[i] == list[j]) {
+                        n++;
+                        checked[j] = true;
+                    }
+                }
+                System.out.println(list[i] + " sayısı " + n + " kere tekrar edildi.");
+                n = 0;
+            }
+        }
+    }
 }
